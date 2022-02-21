dd04b0011c6b23ff3ed2ff23ca570d7b107a6dbf
joe_leon1361 <k020g1147@g.neec.ac.jp>
2022-02-21 16:10:49 +0900

    マージ

--- Japan Game Awards 2022/DebugText.cpp
+++ Japan Game Awards 2022/DebugText.cpp
@@ -13,26 +13,26 @@ DebugText::~DebugText()
 
 void DebugText::Initialize(UINT texnumber)
 {
-	// 全てのスプライトデータについて
+	// 全てのスプライトデータについて About all sprite data
 	for (int i = 0; i < _countof(spriteDatas); i++)
 	{
-		// スプライトを生成する
+		// スプライトを生成する Generate sprites
 		spriteDatas[i] = Sprite::Create(texnumber, { 0,0 });
 	}
 }
 
-// 1文字列追加
+// 1文字列追加 Add 1 string
 void DebugText::Print(const std::string & text, float x, float y, float scale = 1.0f)
 {
-	// 全ての文字について
+	// 全ての文字について About all characters
 	for (int i = 0; i < text.size(); i++)
 	{
-		// 最大文字数超過
+		// 最大文字数超過 Exceeding the maximum number of characters
 		if (spriteIndex >= maxCharCount) {
 			break;
 		}
 
-		// 1文字取り出す(※ASCIIコードでしか成り立たない)
+		// 1文字取り出す(※ASCIIコードでしか成り立たない) Extract one character (* Only valid with ASCII code)
 		const unsigned char& character = text[i];
 
 		int fontIndex = character - 32;
@@ -43,23 +43,23 @@ void DebugText::Print(const std::string & text, float x, float y, float scale = 
 		int fontIndexY = fontIndex / fontLineCount;
 		int fontIndexX = fontIndex % fontLineCount;
 
-		// 座標計算
+		// 座標計算 Coordinate calculation
 		spriteDatas[spriteIndex]->SetPosition({ x + fontWidth * scale * i, y });
 		spriteDatas[spriteIndex]->SetTextureRect({ (float)fontIndexX * fontWidth, (float)fontIndexY * fontHeight }, { (float)fontWidth, (float)fontHeight });
 		spriteDatas[spriteIndex]->SetSize({ fontWidth * scale, fontHeight * scale });
 
-		// 文字を１つ進める
+		// 文字を１つ進める Advance one letter
 		spriteIndex++;
 	}
 }
 
-// まとめて描画
+// まとめて描画 Draw together
 void DebugText::DrawAll(ID3D12GraphicsCommandList* cmdList)
 {
-	// 全ての文字のスプライトについて
+	// 全ての文字のスプライトについて About all character sprites
 	for (int i = 0; i < spriteIndex; i++)
 	{
-		// スプライト描画
+		// スプライト描画 Sprite drawing
 		spriteDatas[i]->Draw();
 	}
 
